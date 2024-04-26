6b1ba7d0a67168878d135f2353934e086d681587
Yogesh Palve <100296461+YogeshDPalve@users.noreply.github.com>
2024-04-26 22:34:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const image = document.querySelector("img"),
+const picture = document.querySelector("img"),
   input = document.querySelector("input"),
   button = document.querySelector("button"),
   api = `https://api.qrserver.com/v1/`,
   api2 = `create-qr-code/?size=150x150&data=`;
 
 button.addEventListener("click", () => {
-  image.src = `${api}${api2}${input.value}`;
+  picture.src = `${api}${api2}${input.value}`;
 });
